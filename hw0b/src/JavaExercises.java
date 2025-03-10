191986d7af8c15eaa170a5cd2e3d3327e59396d4
Acc <Acc@wsl.com>
2025-03-10 17:39:45 +0800

    finished hw0b

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,4 +1,8 @@
+import org.checkerframework.common.value.qual.EnsuresMinLenIf;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 
 public class JavaExercises {
@@ -6,7 +10,10 @@ public class JavaExercises {
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
         // TODO: Fill in this function.
-        return null;
+//        检查出此处有警告，发现是arr并未产生使用所以可以直接创建就返回取消警告
+//        int[] arr = {1,2,3,4,5,6};
+//        return arr;
+        return new int[]{1,2,3,4,5,6};
     }
 
     /** Returns the order depending on the customer.
@@ -14,15 +21,36 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        // 定义不同客户的订单
+        String[] ergunStringArr = {"beyti", "pizza", "hamburger", "tea"};
+        String[] erikStringArr = {"sushi", "pasta", "avocado", "coffee"};
+
+        // 创建一个 Map 来存储客户和订单的映射关系
+        Map<String, String[]> map = new HashMap<>();
+        map.put("Ergun", ergunStringArr);
+        map.put("Erik", erikStringArr);
+
+        // 使用 getOrDefault 方法，如果客户不存在，返回一个空数组
+        return map.getOrDefault(customer, new String[]{"","",""});
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
         // TODO: Fill in this function.
-        return 0;
+        int min = array[0];
+        int max = array[0];
+        for(int i = 1;i < array.length;i++){
+
+            // 这里之所以使用else if是因为注意到一个数不可能又是最大又是最小，因此只会进入一个分支
+            if(array[i] > max){
+                max = array[i];
+            }else if(array[i] < min){
+                min = array[i];
+            }
+        }
+
+        return  max - min;
     }
 
     /**
@@ -39,7 +67,14 @@ public class JavaExercises {
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
         // TODO: Fill in this function.
-        return null;
+        list.add(x);
+        if(x==1){
+            return list;
+        }else if(x%2==0){
+            return hailstoneHelper(x/2,list);
+        }else{
+            return hailstoneHelper(x*3+1,list);
+        }
     }
 
 }
--- hw0b/src/MapExercises.java
+++ hw0b/src/MapExercises.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -7,7 +8,12 @@ public class MapExercises {
      */
     public static Map<Character, Integer> letterToNum() {
         // TODO: Fill in this function.
-        return null;
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c = 'a';c <= 'z';c++){
+            int n = c - 'a' + 1;
+            map.put(c,n);
+        }
+        return map;
     }
 
     /** Returns a map from the integers in the list to their squares. For example, if the input list
@@ -15,12 +21,25 @@ public class MapExercises {
      */
     public static Map<Integer, Integer> squares(List<Integer> nums) {
         // TODO: Fill in this function.
-        return null;
+        Map<Integer, Integer> smap = new HashMap<>();
+        for(int i : nums){
+            smap.put(i,i*i);
+        }
+        return smap;
     }
 
     /** Returns a map of the counts of all words that appear in a list of words. */
     public static Map<String, Integer> countWords(List<String> words) {
         // TODO: Fill in this function.
-        return null;
+        Map<String, Integer> map = new HashMap<>();
+        for(String word : words){
+            if(map.containsKey(word)){
+                int currentVal = map.get(word);
+                map.put(word,currentVal+1);
+            }else{
+                map.put(word,1);
+            }
+        }
+        return map;
     }
 }
