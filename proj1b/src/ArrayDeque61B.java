10f7f974a922c40fc4ee7bd114e7232e38462116
Acc <Acc@wsl.com>
2025-03-23 14:27:19 +0800

    added some Tests for proj1b by Acc and pass the Tests with non-extends ArrayList

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,117 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+
+    //根据提示我们需要使用一个循环数组
+    private T[] arr ;
+    private int size = 0;
+    private int front = 0;
+    private int last = 0;
+    //last 指向的是尾部元素的下一位
+    //front 指向的是头部元素的当前头
+
+
+    public ArrayDeque61B(){
+        //空输入的构造函数
+        arr = (T[]) new Object[1000];
+        size = 0;
+    }
+
+    public ArrayDeque61B(T x){
+        //空输入的构造函数
+        arr = (T[]) new Object[1000];
+        arr[front] = x;
+        last ++;
+        size += 1;
+    }
+
+
+    @Override
+    public void addFirst(T x) {
+        //传入的数字是添加的元素
+        if(Math.floorMod(last + 1,1000) == front){
+            //此时数组超出范围了
+            System.out.println("can not addFirst");
+        }else{
+            front = Math.floorMod(front - 1,1000);//更新front指针
+            arr[front] = x;
+            size ++;
+        }
+    }
+
+    @Override
+    public void addLast(T x) {
+        //传入的数字是添加的元素
+        if(Math.floorMod(last + 1,1000) == front){
+            //此时数组超出范围了
+            System.out.println("can not addLast");
+        }else{
+            arr[last] = x;
+            size ++;
+            last = Math.floorMod(last + 1,1000);//更新front指针
+        }
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> resList = new ArrayList<>();
+        int i = front;
+        while(i != last){
+            resList.add(arr[i]);
+            i = Math.floorMod(i + 1,1000);
+        }
+
+        return resList;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        T removeElement = arr[front];
+        front = Math.floorMod(front + 1,1000);
+        size --;
+        return removeElement;
+    }
+
+    @Override
+    public T removeLast() {
+        T removeElement = arr[last - 1];
+        last = Math.floorMod(last - 1,1000);
+        size --;
+        return removeElement;
+    }
+
+    @Override
+    public T get(int index) {
+        int i = Math.floorMod(front + index,1000);
+        return arr[i];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        T getElement = getRecursiveHelper(index , front);
+        return getElement;
+    }
+
+    //下面这个辅助函数
+    private T getRecursiveHelper(int index, int i){
+        if(index == 0){
+            return arr[i];
+        }else{
+            i = Math.floorMod(i + 1,1000);
+            return getRecursiveHelper(index - 1,i);
+        }
+    }
+}
--- proj1b/tests/ArrayDeque61BTest.java
+++ proj1b/tests/ArrayDeque61BTest.java
@@ -20,4 +20,139 @@ public class ArrayDeque61BTest {
 //         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
 //     }
 
+
+     @Test
+     /* test the add first method */
+     public void addFirstTestBasic(){
+         Deque61B<String> lld1 = new ArrayDeque61B<String>();
+
+
+         lld1.addFirst("!");
+         lld1.addFirst("say");
+         lld1.addFirst("I");
+         lld1.addFirst("can");
+         lld1.addFirst("what");
+         lld1.addFirst(",");
+         lld1.addFirst("Man");
+
+
+
+         assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say","!").inOrder();
+
+     }
+
+    @Test
+    /* test the add first method */
+    public void addLastTestBasic(){
+        Deque61B<String> lld1 = new ArrayDeque61B<String>();
+
+        lld1.addLast("Man");
+        lld1.addLast(",");
+        lld1.addLast("what");
+        lld1.addLast("can");
+        lld1.addLast("I");
+        lld1.addLast("say");
+        lld1.addLast("!");
+
+
+
+
+        assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say","!").inOrder();
+
+    }
+
+    @Test
+    /* test the add last method */
+    public void isEmptyTestBasic(){
+        Deque61B<String> lld1 = new ArrayDeque61B<String>();
+
+        assertThat(lld1.isEmpty()).isTrue();
+
+        lld1.addLast("Man");
+        lld1.addLast(",");
+        lld1.addLast("what");
+        lld1.addLast("can");
+        lld1.addLast("I");
+        lld1.addLast("say");
+        lld1.addLast("!");
+
+
+
+
+        assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say","!").inOrder();
+
+    }
+
+    @Test
+    /* test the add last method */
+    public void sizeTestBasic(){
+        Deque61B<String> lld1 = new ArrayDeque61B<String>("Man");
+
+//        assertThat(lld1.isEmpty()).isTrue();
+
+//        lld1.addLast("Man");
+        lld1.addLast(",");
+        lld1.addLast("what");
+        lld1.addLast("can");
+        lld1.addLast("I");
+        lld1.addLast("say");
+        lld1.addLast("!");
+
+
+
+
+        assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say","!").inOrder();
+
+    }
+
+   @Test
+    /* test the add last method */
+    public void removeLastTestBasic(){
+        Deque61B<String> lld1 = new ArrayDeque61B<String>("Man");
+
+        assertThat(lld1.isEmpty()).isFalse();
+
+//        lld1.addLast("Man");
+        lld1.addLast(",");
+        lld1.addLast("what");
+        lld1.addLast("can");
+        lld1.addLast("I");
+        lld1.addLast("say");
+        lld1.addLast("!");
+
+        String  a = lld1.removeLast();
+
+
+        assertThat(a).isEqualTo("!");
+
+        assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say").inOrder();
+
+    }
+
+    @Test
+    /* test the add last method */
+    public void getTestBasic(){
+        Deque61B<String> lld1 = new ArrayDeque61B<String>("Man");
+
+        assertThat(lld1.isEmpty()).isFalse();
+
+//        lld1.addLast("Man");
+        lld1.addLast(",");
+        lld1.addLast("what");
+        lld1.addLast("can");
+        lld1.addLast("I");
+        lld1.addLast("say");
+        lld1.addLast("!");
+
+        String  a = lld1.getRecursive(2);
+
+
+        assertThat(a).isEqualTo("what");
+
+//        assertThat(lld1.toList()).containsExactly("Man",",","what","can","I","say").inOrder();
+
+    }
+
+
+
 }
